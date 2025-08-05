07122604e6bc29a4fc30af7921e4c989926ed03d
wokovn <nakhoa231@clc.fitus.edu.vn>
2025-08-05 21:57:07 +0700

    update search for review

--- Frontend/src/components/Reviews/index.js
+++
@@ -1,5 +0,0 @@
-// Reviews components barrel export
-export { default as StarRating } from './StarRating';
-export { default as ReviewItem } from './ReviewItem';
-export { default as RatingDistribution } from './RatingDistribution';
-export { default as ReviewSection } from './ReviewSection';
